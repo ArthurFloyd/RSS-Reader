1540d66b7776944762210c7ef3f922f66de2eda2
arthur voryshev <what_to_write@mail.ru>
2024-01-09 06:33:14 +0500

    install Webpack and Bootstrap

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ install:
 	npm ci
 lint:
 	npx eslint .
+develop:
+	npx webpack serve
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
