17f0e29d3161e3d534f3d2191130a87ad64ac702
arthur voryshev <what_to_write@mail.ru>
2024-03-08 00:57:28 +1000

    add locales

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import * as yup from 'yup';
+import { setLocale, string } from 'yup';
+import onChange from 'on-change';
+import i18n from 'i18next';
+
+import render from './view';
+import ru from './locales/ru.js';
 
 // Валидатор
 const validate = (url, links) => {
@@ -6,3 +11,47 @@ const validate = (url, links) => {
     .notOneOf(links);
   return schema.validate(url);
 };
+
+// Приложение
+const app = () => {
+  const i18nextInstance = i18n.createInstance();
+  // Инициализация
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources: { ru },
+  });
+  // Элементы
+  const element = {
+    form: document.querySelector('.rss-form'),
+    dtn: document.querySelector('button[type="submit"]'),
+  };
+
+  setLocale({
+    mixed: { notOneOf: 'alreadyAddedRSS' },
+    string: { url: 'invalidUrl', requared: 'mustNotBeEmpty' },
+  });
+
+  // Состояние
+  const state = {
+    isValidForm: {
+      valide: null,
+      error: [],
+    },
+    content: {
+      feeds: [],
+      posts: [],
+    },
+  };
+  const watchedState = onChange(state, render(state, element));
+
+  element.form.focus();
+  element.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(element.form);
+    const url = formData.get('url');
+    const addedLinks = watchedState.content.feeds.map(({ link }) => link);
+  });
+};
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.scss';
 import 'bootstrap';
+import app from './app';
 
-console.log('Hello, World!');
+app();
